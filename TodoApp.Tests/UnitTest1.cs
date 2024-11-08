c1252c1843ff73c528e00e8b990a9a08547bb5e6
b-bastian <bbrugger@schueler.hakzell.at>
2024-11-08 08:56:27 +0100

    added tests

---
+++ TodoApp.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using TodoApp.Core;
+using TodoApp.Core.ViewModels;
+using TodoApp.TodoData;
+
+namespace TodoApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void TestVM()
+    {
+        // arrange
+        IRepository rep = new StaticData();
+        
+        // act
+        MainViewModel viewModel = new MainViewModel(rep);
+
+        // assert
+        Assert.IsNotNull(viewModel);
+    }
+}
